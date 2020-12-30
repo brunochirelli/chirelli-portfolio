174e090064919777c2009657055214170c00de3b
Bruno <brunochirelli@gmail.com>
2020-12-30 14:05:02 -0300

    extract contact infos to its own component

--- src/pages/contato.tsx
+++ src/pages/contato.tsx
@@ -1,26 +1,21 @@
 import React from "react";
 
-import { Box, Container, IconButton } from "@material-ui/core";
+import { Box, Container, IconButton, Typography } from "@material-ui/core";
 import { GitHub, LinkedIn } from "@material-ui/icons";
 
 import Layout from "../components/Layout";
 import PageHeader from "../components/PageHeader";
+import ContactInfos from "../components/ContactInfos";
 
 const Contact = () => {
   return (
     <Layout pageLabel="Contato">
-      <PageHeader title="Entre em Contato" />
-      <Container>
-        <Box>
-          <IconButton href="https://google.com.br">
-            <GitHub />
-          </IconButton>
-          <IconButton href="https://google.com.br">
-            <LinkedIn />
-          </IconButton>
-        </Box>
-        <Box>brunochirelli@gmail.com</Box>
-      </Container>
+      <PageHeader
+        title="Entre em Contato"
+        lead="Estarei ansioso para conversar com você"
+      />
+
+      <ContactInfos />
     </Layout>
   );
 };
