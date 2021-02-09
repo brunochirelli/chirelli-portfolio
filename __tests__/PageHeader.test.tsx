d113cc90ed19bb2ac54d6a12fbe7c15849d15e70
Bruno <brunochirelli@gmail.com>
2021-02-09 17:23:03 -0300

    add header tests

--- __tests__/PageHeader.test.tsx
+++ __tests__/PageHeader.test.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import PageHeader from "../src/components/PageHeader";
 import { render } from "../src/utils/test-utils";
 
-test("it renders", () => {
-  const { getByTestId } = render(<PageHeader title="Page Title" />);
-  expect(getByTestId("page-header")).toHaveTextContent("Page Title");
+describe("PageHeader basic functionalities", () => {
+  it("should render right title", () => {
+    const { getByTestId } = render(<PageHeader title="Page Title" />);
+    expect(getByTestId("page-header")).toHaveTextContent("Page Title");
+  });
+
+  it("should distinguish between project types", () => {});
+  it("should link right to external sources", () => {});
+  it("should not display links when value is falsy", () => {});
 });
