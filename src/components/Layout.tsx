ee3b599d84d8e580d0577a25e3c5505b95a9aff8
Bruno <brunochirelli@gmail.com>
2021-02-09 17:23:57 -0300

    update page label handling and place up on SSR and Browser

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "@reach/router";
 
-import Footer from "./Footer";
+// import Footer from "./Footer";
 import Header from "./Header";
 
 import { LayoutStyled } from "./Layout.styled";
@@ -10,12 +10,19 @@ export declare type Lang = "EN" | "PT-BR";
 
 type LayoutProps = {
   children: React.ReactNode;
-  location?: object;
+  location?: any;
 };
 
 const Layout = ({ children, location }: LayoutProps) => {
   const [open, setOpen] = useState<boolean>(false);
   const [lang, setLang] = useState<Lang>("EN");
+
+  /** Get actual location of the page */
+  let page = location.pathname;
+
+  /** Handle page name to output as a label */
+  let pageName =
+    page === "/" ? "Home" : page.replace(/\/./, page[1].toUpperCase());
 
   const handleOpen = (): void => {
     setOpen(true);
@@ -26,13 +33,14 @@ const Layout = ({ children, location }: LayoutProps) => {
   };
 
   useEffect(() => {
+    /** Every time that location updates, close the menu */
     return handleClose();
   }, [location]);
 
   return (
     <LayoutStyled>
       <Header
-        pageLabel={""}
+        pageLabel={pageName}
         open={open}
         onOpen={handleOpen}
         onClose={handleClose}
