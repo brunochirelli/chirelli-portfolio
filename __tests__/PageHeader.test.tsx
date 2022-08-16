568c2f3e884324ffda7bce424e2edd87d9fa2138
Bruno Chirelli <brunochirelli@gmail.com>
2022-08-16 13:43:41 -0300

    Remove previous files to start monorepo fresh

--- __tests__/PageHeader.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PageHeader from "../src/components/PageHeader";
-import { render } from "../src/utils/test-utils";
-
-test("it renders", () => {
-  const { getByTestId } = render(<PageHeader title="Page Title" />);
-  expect(getByTestId("page-header")).toHaveTextContent("Page Title");
-});
